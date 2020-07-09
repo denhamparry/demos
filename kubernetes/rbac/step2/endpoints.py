565416f6c23a0c664f5aa63d1d64fcebb0850db3
Lewis Denham-Parry <lewis@control-plane.io>
2020-07-09 18:29:50 +0100

    feat: rbac workshop
    - introduce concept of a user
    - show roles
    - show rolebindings
    - show clusterrole
    - show clusterrolebindings
    - show rbac in an application locallly
    - show rbac in a deployed application
    - reference tools to help out

--- kubernetes/rbac/step2/endpoints.py
+++
@@ -1,8 +0,0 @@
-from kubernetes import client, config
-
-config.load_kube_config()
-
-v1=client.CoreV1Api()
-print("Listing endpoints in the namespace development:")
-api_response = v1.list_namespaced_endpoints(namespace="development", watch=False)
-print(api_response)
---
+++ kubernetes/rbac/step5/README.md
@@ -0,0 +1,9 @@
+# Tools
+
+## RBAC Tool
+
+https://github.com/alcideio/rbac-tool
+
+## Permission Manager
+
+https://github.com/sighupio/permission-manager
